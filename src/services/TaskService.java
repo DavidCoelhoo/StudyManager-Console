7d29ffbb96cb4681f53e516630b802ff5e3ad3e2
David Coelho <davidmcoelho18@gmail.com>
2026-07-18 13:07:05 -0300

    Add TaskService with CRUD operations

---
+++ src/services/TaskService.java
@@ -0,0 +1,14 @@
+package services;
+
+import entities.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskServices {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public TaskServices(List<Task> tasks){
+
+    }
+}
